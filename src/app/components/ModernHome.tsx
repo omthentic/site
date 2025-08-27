a40546915112d5bd42eb63e072f85360b7b73f82
ShanakaJayakody <shanaka@medwithpurpose.com>
2025-08-27 10:04:23 +1000

    hero video

--- src/app/components/ModernHome.tsx
+++ src/app/components/ModernHome.tsx
@@ -17,7 +17,7 @@ export default function ModernHome() {
         {/* Background Video */}
         <div className="absolute inset-0 z-0">
           <video
-            src="/videos/4K Hero Background v2.1.mp4"
+            src="/videos/4K Hero_Background v2.1.mp4"
             className="w-full h-full object-cover"
             style={{
               position: 'absolute',
